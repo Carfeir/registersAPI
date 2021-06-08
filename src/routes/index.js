aabc27bd6b170ca769643995f62bc63315bb802c
Carfeir <mauiripasian@gmail.com>
2021-06-08 12:40:29 -0300

    master

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,9 @@ const { isLoggedIn } = require('../lib/auth');
 
 router.get('/', isLoggedIn, async (req, res) => {
     const registers = await pool.query('SELECT * FROM registers WHERE user_id = ? LIMIT 10', [req.user.id]);
-    //const balance = await pool.query('select ((SELECT sum(amount) AS income FROM registers WHERE user_id = 1 AND type = "income") - (SELECT sum(amount) AS outgoing FROM registers WHERE user_id = 1 AND type = "outgoing")) as totalAmount', [req.user.id], [req.user.id]);
-    res.render('index', { registers } );
+    const balance = await pool.query('SELECT SUM(if(type ="income", amount, 0)) AS income, SUM(if(type ="outgoing", amount ,0)) AS outgoing, SUM(if(type="income",amount,-1*amount)) AS balance FROM registers WHERE user_id = ?', [req.user.id]);
+    const balanceResult = balance.map(({ balance }) => balance);
+    res.render('index', { registers, balanceResult } );
 });
 
 module.exports = router;
